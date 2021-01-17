18ffe36a43d5c502d961999012661fc47710d5cf
himmer <himmer@himmers-MacBook-Pro.local>
2021-01-17 00:03:44 -0600

    copyright

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -117,10 +117,7 @@ export class Contact extends Component {
         <div id="footer">
           <div className="container text-center">
             <p>
-              &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
-              <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
-              </a>
+              &copy; 2021 Abram Himmer
             </p>
           </div>
         </div>
