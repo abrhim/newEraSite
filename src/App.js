d77f119efded93269e93b46d4f4c8f58f3adb1ec
Abram Himmer <abramhimmer@gmail.com>
2021-04-17 11:39:21 -0500

    push

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <Nav />
       <Hero />
-      <Services />
       <About />
+      <Services />
       <Clients />
       <Gallery />
       <Contact />
--- src/components/Services/data.js
+++ src/components/Services/data.js
@@ -1,6 +1,15 @@
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.";
 export const services = [
+  { title: "Yard Grading", img: "img/services/yardGrading2.jpg", paragraph },
+  {
+    title: "Site Maintenance",
+    img: "img/services/siteMaintenance2.jpg",
+    paragraph:
+      "We specialize in frame cleaning, drywall cleaning, masonry clean, and final clean.",
+  },
+  { title: "Brush Clearing", img: "img/services/brushClearing.jpg", paragraph },
+
   {
     title: "Concrete Driveways",
     img: "img/services/ConcreteDriveways.jpg",
@@ -16,12 +25,4 @@ export const services = [
     img: "img/services/CustomConcrete.jpg",
     paragraph,
   },
-  { title: "Brush Clearing", img: "img/services/brushClearing.jpg", paragraph },
-  {
-    title: "Site Maintenance",
-    img: "img/services/siteMaintenance2.jpg",
-    paragraph:
-      "We specialize in frame cleaning, drywall cleaning, masonry clean, and final clean.",
-  },
-  { title: "Yard Grading", img: "img/services/yardGrading2.jpg", paragraph },
 ];
